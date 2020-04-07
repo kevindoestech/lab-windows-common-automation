6965537e1b3972e277c46f290482ea168e97c6b6
K Ma - t1 <t1-mak@ad.mak.net>
2020-04-06 21:06:50 -0700

    Add Module Installation

--- MyWindowsUpdates.ps1
+++ MyWindowsUpdates.ps1
@@ -1,8 +1,19 @@
-﻿#Downloads All Available Updates
+﻿#Install PSWindowsUpdate Module
+$myModule = 'PSWindowsUpdate'
+
+try {
+    Import-Module -Name $myModule
+}
+catch {
+    Install-Module -Name $myModule
+    Import-Module -Name $myModule
+}
+finally {
+    Get-Module -Name $myModule
+}
+
+#Downloads All Available Updates
 Get-WindowsUpdate -Download -AcceptAll
 
 #Installs All Available Updates
 Get-WindowsUpdate -Install -AcceptAll
-
-#Gets History
-Get-WUHistory
